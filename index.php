3c289c9b7620ebb4bc5776fd0d228b6eef73fbe9
ddetoni <ddetoni@thoughtworks.com>
2016-05-05 18:05:03 -0300

    Changes app to run on nginx.

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<link rel="stylesheet" href="styles/style1.css" type="text/css">
 
-	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"></script>  
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"></script>
     <script type="text/javascript">
     $(document).ready(function() {
 
@@ -41,6 +41,8 @@
 	<div id=login>
 
 		<?php
+		require('vendor/autoload.php');
+
 		session_start();
 		if($_SESSION[validation] != "1")
 		{
