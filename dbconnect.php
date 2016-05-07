042cb6d48cbde1de53604d8cdb27451f57b73185
ddetoni <ddetoni@thoughtworks.com>
2016-05-07 12:16:08 -0300

    Configs to remote database connection.

--- dbconnect.php
+++ dbconnect.php
@@ -1,12 +1,12 @@
 <?php
 
-	$hostname = 'localhost';
-	$user = 'root';
-	$pass = 'tarifa185';
-	$dbase = 'social_news';
-	
+	$hostname = 'us-cdbr-iron-east-03.cleardb.net';
+	$user = 'b3c1054bc0b5c8';
+	$pass = 'd7a39280';
+	$dbase = 'heroku_93a35212af060ff';
+
 	$db = mysql_connect($hostname,$user,$pass);
 
 	mysql_select_db($dbase);
-	
+
 ?>
--- login.php
+++ login.php
@@ -3,13 +3,13 @@
 
 	$username = $_POST['username'];
 	$password = sha1($_POST['password']);
- 
+
 	include 'dbconnect.php';
-	
+
 	$sql = "SELECT * FROM Users WHERE username='".$username."'";
 	$query = mysql_query($sql);
 	$sql = mysql_fetch_array($query);
-	
+
 	if ($sql[password] == $password)
 	{
 
@@ -18,14 +18,14 @@
 		$username = $_POST['username'];
 
 		session_start();
-	
+
 		$_SESSION[username] = $username;
 		$_SESSION[validation] = $validation;
 		$_SESSION[permitionLevel] = $sql[permitionLevel];
 		$_SESSION[idUser] = $sql[idUsers];
-		
+
 		header ("Location: index.php");
 	}
-	
+
 	header ("Location: index.php");
 ?>
