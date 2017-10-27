67889b5c54ade67e8f156192b12c1b92d2d86734
Douglas Detoni <ddetoni@thoughtworks.com>
2017-10-27 14:46:51 -0300

    Migrate edit_news to use API

--- src/edit_news.php
+++ src/edit_news.php
@@ -1,17 +1,19 @@
 <?php
-	include 'dbconnect.php';
+	require "../vendor/autoload.php";
+	session_start();
+
+	use Resty\Resty;
+
+	$resty = new Resty();
+	$resty->setBaseURL(getenv('API_BASE_URL'));
 	
 	$idNew = $_GET[idNew];
-	
-	$query = "SELECT title, text, tags FROM News WHERE idNews=$idNew";
-	$data = mysql_query($query);
-	
-	$new = mysql_fetch_array($data);
+	$response = $resty->get("news/$idNew");
 ?>
 
 <form name="add_news" action="update_new.php?idNew=<?php echo $idNew; ?>" method="post">
-	Title: <input type="text" name="title" value="<?php echo $new[title]; ?>"><br>
-	Text:<br> <textarea name="text" rows="20" cols="60"><?php echo $new[text]; ?></textarea><br>
-	Tags: <input type="text" name="tags" value="<?php echo $new[tags]; ?>"><br>
+	Title: <input type="text" name="title" value="<?php echo $response['body']->title; ?>"><br>
+	Text:<br> <textarea name="text" rows="20" cols="60"><?php echo $response['body']->text; ?></textarea><br>
+	Tags: <input type="text" name="tags" value="<?php echo $response['body']->tags; ?>"><br>
 	<button type="submit" name="send_new">Update New</button>
 </form>
