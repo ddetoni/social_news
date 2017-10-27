a5b89b547598eea97bab3cafd67e60089fa93402
Douglas Detoni <ddetoni@thoughtworks.com>
2017-10-27 15:46:57 -0300

    Migrate my_news to use API

--- src/my_news.php
+++ src/my_news.php
@@ -1,19 +1,21 @@
 <?php
+	require "../vendor/autoload.php";
 	session_start();
-	include 'dbconnect.php';
 	
-	$query = "SELECT idNews, title, url FROM News WHERE Users_idUsers='$_SESSION[idUser]' ORDER BY date DESC";
-	$data = mysql_query($query) or die(mysql_error());
+	use Resty\Resty;
+
+	$resty = new Resty();
+	$resty->setBaseURL(getenv('API_BASE_URL'));
 	
-	while($row = mysql_fetch_array($data))
-	{
-		
+	$idUser = $_SESSION[idUser];
+	$response = $resty->get("news", "filter[where][Users_idUsers]=$idUser&filter[order]=date%20DESC");
+
+	foreach($response['body'] as $new) {
 		echo "<div id='my_new'> 
-				<a id='new' href='$row[url]'><h4>$row[title]</h4></a>
-				<a id='edit_new' href='#' onclick='editNew($row[idNews])'>Edit</a> <br>
-				<a id='edit_new' href='#' onclick='removeNew($row[idNews])'>Remove</a>
+				<a id='new' href='$new->url'><h4>$new->title</h4></a>
+				<a id='edit_new' href='#' onclick='editNew($new->idNews)'>Edit</a> <br>
+				<a id='edit_new' href='#' onclick='removeNew($new->idNews)'>Remove</a>
 				<br>
 			</div> ";
-		
 	}
 ?>
